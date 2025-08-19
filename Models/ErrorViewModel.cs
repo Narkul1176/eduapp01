829e4ec38e989ed6ba916a4deedfe6e5cce8408f
Raghavendra K <raghaav9@gmail.com>
2025-08-19 19:08:02 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eduapp01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
